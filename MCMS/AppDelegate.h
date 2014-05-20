9bbc978c0e028c90b14a87ac2490437dd15db84f
Kristen Fisher <kristen.fisher@d214.org>
2014-05-20 13:28:07 -0500

    Initial Commit

---
+++ MCMS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MCMS
+//
+//  Created by Kristen L. Fisher on 5/20/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
